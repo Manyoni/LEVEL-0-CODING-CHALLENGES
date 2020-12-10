457783390f22be46bcf807a521b4c1af64e92d1f
Manyoni <74727483+Manyoni@users.noreply.github.com>
2020-12-10 16:20:57 +0200

    Update task 4.py

--- task 4.py
+++ task 4.py
@@ -1,10 +1,7 @@
 def test_number_3 (x,y):
-    total =x+y
+    total = x+y
     sum_list= list(str(total))
-    if x == 3 or y == 3  and "3" in sum_list:
+    if (x == 3 or y == 3) and "3" in sum_list:
         return True
     else:
         return False
-
-print(test_number_3(9,4))
-
