86a44396736b2366412080a396b2cb0fbc3cdcfa
Manyoni <74727483+Manyoni@users.noreply.github.com>
2020-12-10 13:02:43 +0200

    First commit

--- task 11.py
+++ task 11.py
@@ -1,15 +1,16 @@
 def printing_common_characters(str1,str2):
     a = list(set(str1)&set(str2))
-    characters_list =[]
-    character=0
-
+    string =""   
     for i in a:
-         character = character+1
-         characters_list.append(i)
-    return characters_list
+        string +=i
+    return string
+    
 
 a = printing_common_characters("house","computer")
 print ("Common letters:",a)
+
+
+
 
 
 
